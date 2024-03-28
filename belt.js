d42b03853a6faefada7950691414490f888cdda4
amhss2000 <165250777+amhss2000@users.noreply.github.com>
2024-03-28 23:04:18 +0530

    1

--- belt.js
+++ belt.js
@@ -1,11 +1,7 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzFEWdZxMd8HbQmLnzzMj7Ki55AXwn85v7eRLGQyaucyYfbQ9kyTdDF5GCYFyGZdQXrdg/exec'
-
-const form = document.forms['Belt Distribution']
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Given input is entered to us!!! Thank You" ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+function doPost(e) {
+  const sheet = SpreadsheetApp.openById('https://docs.google.com/spreadsheets/d/1FFETqlVPnIUC_Fphw1-bP7GSTJAlaQ7ze5ge9AnQdLk/edit#gid=0').getActiveSheet();
+  const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
+  const newRow = headers.map(header => e.parameter[header] || '');
+  sheet.appendRow(newRow);
+  return ContentService.createTextOutput(JSON.stringify({ status: 'success' })).setMimeType(ContentService.MimeType.JSON);
+}
