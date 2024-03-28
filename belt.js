f0bdc9cfa1322d49549dcaa4b4655ae5e6672f52
amhss2000 <165250777+amhss2000@users.noreply.github.com>
2024-03-28 23:07:16 +0530

    Delete belt.js

--- belt.js
+++
@@ -1,7 +0,0 @@
-function doPost(e) {
-  const sheet = SpreadsheetApp.openById('https://docs.google.com/spreadsheets/d/1FFETqlVPnIUC_Fphw1-bP7GSTJAlaQ7ze5ge9AnQdLk/edit#gid=0').getActiveSheet();
-  const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
-  const newRow = headers.map(header => e.parameter[header] || '');
-  sheet.appendRow(newRow);
-  return ContentService.createTextOutput(JSON.stringify({ status: 'success' })).setMimeType(ContentService.MimeType.JSON);
-}
