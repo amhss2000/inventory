8e579b1cff8dd0ee4313f91ac58ef514007fd685
amhss2000 <165250777+amhss2000@users.noreply.github.com>
2024-04-03 13:10:06 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$username = 'your_username';
-$password = 'your_password';
-$database = 'school_inventory';
+$host = 'sql113.infinityfree.com';
+$username = 'if0_36256603';
+$password = 'amhss2000';
+$database = 'if0_36256603_inventory1';
 
 // Create connection
 $connection = mysqli_connect($host, $username, $password, $database);
